041e50b5462b312e052e12113c416e8993e1f331
Timothy Campbell <tcampbel@student.hive.fi>
2024-10-30 17:58:37 +0200

    Added nested iterator class to header

--- module_08/ex02/MutantStack.hpp
+++ module_08/ex02/MutantStack.hpp
@@ -6,36 +6,40 @@
 /*   By: tcampbel <tcampbel@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 11:58:18 by tcampbel          #+#    #+#             */
-/*   Updated: 2024/10/30 12:18:18 by tcampbel         ###   ########.fr       */
+/*   Updated: 2024/10/30 17:52:29 by tcampbel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
+
+#include <iostream>
+#include <vector>
+
+template<typename T>
 class MutantStack
 {
+private:
+	std::vector<T>	stack;
 public:
 	MutantStack();
 	MutantStack(const MutantStack& copy);
 	const MutantStack&	operator=(const MutantStack& other);
 	~MutantStack();
-	
+	void	pop() const; //removes the top element
+	bool	empty() const; //checks whether the container adaptor is empt
+	T*		top() const; //accesses the top element
+	size_t	size() const; //returns the number of elements
+	void	push(T& val) //inserts element at the top
+	class Iterator
+	{
+		private:
+			std::vector<T>::iterator vIt;
+		public:
+			Iterator();
+			Iterator(const Iterator& copy);
+			~Iterator();
+			
+	}
 };
 
-
-top -> accesses the top element
-
-
-empty -> checks whether the container adaptor is empt
-
-size -> returns the number of elements
-
-push -> inserts element at the top
-
-push_range -> inserts a range of elements at the top
-
-emplace -> constructs element in-place at the top
-
-pop -> removes the top element
-
-swap -> swaps the contents

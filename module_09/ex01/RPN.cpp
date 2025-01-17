976cb481007e7dc5019e6e3b9a650ead236a9337
Timothy Campbell <tcampbel@student.hive.fi>
2025-01-17 17:58:54 +0200

    Fixed division, subract needs work

--- module_09/ex01/RPN.cpp
+++ module_09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: tcampbel <tcampbel@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 14:35:57 by tcampbel          #+#    #+#             */
-/*   Updated: 2025/01/17 17:51:46 by tcampbel         ###   ########.fr       */
+/*   Updated: 2025/01/17 17:57:38 by tcampbel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,11 +85,17 @@ void RPN::divide() //needs work to handle 0 and not keep dividing
 {
 	if (stack.size() <  2)
 		throw std::runtime_error("error: insufficient values to perform operation");
-	result = stack.top();
+	int n1 = stack.top();
 	stack.pop();
+	int n2 = stack.top();
+	stack.pop();
+	if (n1 == 0 || n2 == 0)
+		throw  std::invalid_argument("error: cannot divide by 0");
+	result = n2 / n1;
 	while (stack.size() != 0)
 	{
-		result /= stack.top();
+		int n3 = stack.top();
+		result /= n3;
 		stack.pop();
 	}
 	stack.push(result);
